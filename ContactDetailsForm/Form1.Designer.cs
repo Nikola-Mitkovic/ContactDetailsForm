3302fc73aca0e1bbee2a738df203924f0cee6e68
Nikola-Mitkovic <ektim35@gmail.com>
2022-10-31 01:36:16 +0100

    Add project files.

---
+++ ContactDetailsForm/Form1.Designer.cs
@@ -0,0 +1,320 @@
+﻿namespace ContactDetailsForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.clmName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmSurname = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmStreet = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmCity = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmState = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmZIP = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.userBox = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtSurname = new System.Windows.Forms.TextBox();
+            this.txtStreet = new System.Windows.Forms.TextBox();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.txtState = new System.Windows.Forms.TextBox();
+            this.txtZip = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.userBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitter1
+            // 
+            this.splitter1.Location = new System.Drawing.Point(0, 0);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(3, 856);
+            this.splitter1.TabIndex = 14;
+            this.splitter1.TabStop = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGridView1.ColumnHeadersHeight = 46;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.clmName,
+            this.clmSurname,
+            this.clmStreet,
+            this.clmCity,
+            this.clmState,
+            this.clmZIP});
+            this.dataGridView1.Location = new System.Drawing.Point(405, 24);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidth = 82;
+            this.dataGridView1.RowTemplate.Height = 33;
+            this.dataGridView1.Size = new System.Drawing.Size(1303, 803);
+            this.dataGridView1.TabIndex = 16;
+            // 
+            // clmName
+            // 
+            this.clmName.Frozen = true;
+            this.clmName.HeaderText = "Name";
+            this.clmName.MinimumWidth = 10;
+            this.clmName.Name = "clmName";
+            this.clmName.ReadOnly = true;
+            this.clmName.Width = 150;
+            // 
+            // clmSurname
+            // 
+            this.clmSurname.Frozen = true;
+            this.clmSurname.HeaderText = "Surname";
+            this.clmSurname.MinimumWidth = 10;
+            this.clmSurname.Name = "clmSurname";
+            this.clmSurname.ReadOnly = true;
+            this.clmSurname.Width = 300;
+            // 
+            // clmStreet
+            // 
+            this.clmStreet.Frozen = true;
+            this.clmStreet.HeaderText = "Street";
+            this.clmStreet.MinimumWidth = 10;
+            this.clmStreet.Name = "clmStreet";
+            this.clmStreet.ReadOnly = true;
+            this.clmStreet.Width = 300;
+            // 
+            // clmCity
+            // 
+            this.clmCity.Frozen = true;
+            this.clmCity.HeaderText = "City";
+            this.clmCity.MinimumWidth = 10;
+            this.clmCity.Name = "clmCity";
+            this.clmCity.ReadOnly = true;
+            this.clmCity.Width = 200;
+            // 
+            // clmState
+            // 
+            this.clmState.Frozen = true;
+            this.clmState.HeaderText = "State";
+            this.clmState.MinimumWidth = 10;
+            this.clmState.Name = "clmState";
+            this.clmState.ReadOnly = true;
+            this.clmState.Width = 200;
+            // 
+            // clmZIP
+            // 
+            this.clmZIP.Frozen = true;
+            this.clmZIP.HeaderText = "ZIP";
+            this.clmZIP.MinimumWidth = 10;
+            this.clmZIP.Name = "clmZIP";
+            this.clmZIP.ReadOnly = true;
+            this.clmZIP.Width = 150;
+            // 
+            // userBox
+            // 
+            this.userBox.Controls.Add(this.btnSave);
+            this.userBox.Controls.Add(this.txtZip);
+            this.userBox.Controls.Add(this.txtState);
+            this.userBox.Controls.Add(this.txtCity);
+            this.userBox.Controls.Add(this.txtStreet);
+            this.userBox.Controls.Add(this.txtSurname);
+            this.userBox.Controls.Add(this.txtName);
+            this.userBox.Controls.Add(this.label6);
+            this.userBox.Controls.Add(this.label5);
+            this.userBox.Controls.Add(this.label4);
+            this.userBox.Controls.Add(this.label3);
+            this.userBox.Controls.Add(this.label2);
+            this.userBox.Controls.Add(this.label1);
+            this.userBox.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.userBox.Location = new System.Drawing.Point(9, 24);
+            this.userBox.Name = "userBox";
+            this.userBox.Size = new System.Drawing.Size(380, 438);
+            this.userBox.TabIndex = 17;
+            this.userBox.TabStop = false;
+            this.userBox.Text = "Enter Contact Details:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(6, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.Control;
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label2.Location = new System.Drawing.Point(6, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Surname";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label3.Location = new System.Drawing.Point(6, 145);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Street";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label4.Location = new System.Drawing.Point(6, 187);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "City";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label5.Location = new System.Drawing.Point(6, 230);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 25);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "State";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label6.Location = new System.Drawing.Point(6, 269);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 25);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "ZIP";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(110, 55);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(264, 31);
+            this.txtName.TabIndex = 6;
+            // 
+            // txtSurname
+            // 
+            this.txtSurname.Location = new System.Drawing.Point(110, 96);
+            this.txtSurname.Name = "txtSurname";
+            this.txtSurname.Size = new System.Drawing.Size(264, 31);
+            this.txtSurname.TabIndex = 7;
+            // 
+            // txtStreet
+            // 
+            this.txtStreet.Location = new System.Drawing.Point(110, 139);
+            this.txtStreet.Name = "txtStreet";
+            this.txtStreet.Size = new System.Drawing.Size(264, 31);
+            this.txtStreet.TabIndex = 8;
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(110, 181);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(264, 31);
+            this.txtCity.TabIndex = 9;
+            // 
+            // txtState
+            // 
+            this.txtState.Location = new System.Drawing.Point(110, 224);
+            this.txtState.Name = "txtState";
+            this.txtState.Size = new System.Drawing.Size(264, 31);
+            this.txtState.TabIndex = 10;
+            // 
+            // txtZip
+            // 
+            this.txtZip.Location = new System.Drawing.Point(110, 263);
+            this.txtZip.Name = "txtZip";
+            this.txtZip.Size = new System.Drawing.Size(264, 31);
+            this.txtZip.TabIndex = 11;
+            // 
+            // btnSave
+            // 
+            this.btnSave.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnSave.Location = new System.Drawing.Point(110, 340);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(264, 56);
+            this.btnSave.TabIndex = 12;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1730, 856);
+            this.Controls.Add(this.userBox);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.splitter1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.userBox.ResumeLayout(false);
+            this.userBox.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Splitter splitter1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmSurname;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmStreet;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmCity;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmState;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmZIP;
+        private System.Windows.Forms.GroupBox userBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtZip;
+        private System.Windows.Forms.TextBox txtState;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.TextBox txtStreet;
+        private System.Windows.Forms.TextBox txtSurname;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ ContactDetailsForm/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ContactDetailsForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridView1.ColumnCount = 6;
+            dataGridView1.Columns[0].Name = "Name";
+            dataGridView1.Columns[1].Name = "Surname";
+            dataGridView1.Columns[2].Name = "Street";
+            dataGridView1.Columns[3].Name = "City";
+            dataGridView1.Columns[4].Name = "State";
+            dataGridView1.Columns[5].Name = "Zip";
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Add(txtName.Text, txtSurname.Text, txtStreet.Text, txtCity.Text, txtState.Text, txtZip.Text);
+        }
+    }
+}
